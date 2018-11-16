53d1b8c5cfda7fb6ef90cf40a69eb06c98653e99
SeanByungyoonKim <sean.b.kim@edu.uwaterloo.ca>
2018-11-15 21:20:25 -0500

    Add files via upload

--- turtlebot_example_node_lab2.cpp
+++ turtlebot_example_node_lab2.cpp
@@ -12,12 +12,15 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <tf/transform_datatypes.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Path.h>
 #include <sensor_msgs/LaserScan.h>
+#include <visualization_msgs/Marker.h>
 #include <eigen3/Eigen/Dense>
 #include <std_msgs/String.h>
 
@@ -29,9 +32,9 @@
 #include <tf/transform_datatypes.h>
 #include <tf/transform_broadcaster.h>
 
-#define M 300
-#define N 300
-#define RESOLUTION 0.11
+#define M 650
+#define N 650
+#define RESOLUTION 0.02
 
 using namespace Eigen;
 
@@ -66,18 +69,24 @@ void bresenham(int x0, int y0, int x1, int y1, std::vector<int> &x, std::vector<
 // raytracing with Bresenham's algorithm, assigns low probability of object
 // on ray, high probability at end.  Returns cells and probabilities.
 void inversescannerbres(Vector3d &x, float phi, float range, std::vector<int> &linex, std::vector<int> &liney, std::vector<float> &linepm){//MatrixXf &invmod) {
-	//if (range < range_min || range > range_max) return;
-	if (std::isnan(range)) return;
+	// if (std::isnan(range)) {
+	// 	// ROS_INFO("Range is nan");
+	// 	return;
+	// }
+	// if (range < range_min || range > range_max) {
+	// 	return;
+	// }
+
 	auto angle = phi + x(2);
 	// Range finder inverse measurement model
 	// int x1 = max(0.0, min((double)M, x(0)/RESOLUTION));
-	int x1 = max(0.0, min((double)M-1, x(0)/RESOLUTION+M/2)); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
-	int y1 = max(0.0, min((double)N-1, x(1)/RESOLUTION+N/2)); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
+	int x1 = max(0.0, min((double)M-1, x(0)/RESOLUTION+(M/2) )); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
+	int y1 = max(0.0, min((double)N-1, x(1)/RESOLUTION+(N/2) )); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
 	auto endptx = (x(0) + range*cos(angle))/RESOLUTION;
 	auto endpty = (x(1) + range*sin(angle))/RESOLUTION;
 
-	int x2 = max(0.0, min((double)M-1, endptx)+M/2); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
-	int y2 = max(0.0, min((double)N-1, endpty)+N/2); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
+	int x2 = max(0.0, min((double)M-1, endptx+(M/2) ) ); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
+	int y2 = max(0.0, min((double)N-1, endpty+(N/2) ) ); //Adding M/2 as an offset since the bottom left-hand corner is 0,0
 
 	bresenham(x1,y1,x2,y2, linex, liney);
 	//invmod.resize(linex.size(), 3);
@@ -101,7 +110,15 @@ void map_update(Vector3d &x) {
 		// 	continue;
 		// }
 
+		// if (std::isnan(scanner_msg.ranges[i])) {
+		// 	continue;
+		// }
+
 		if (std::isnan(scanner_msg.ranges[i])) {
+		// ROS_INFO("Range is nan");
+			continue;
+		}
+		if (scanner_msg.ranges[i] < range_min || scanner_msg.ranges[i] > range_max) {
 			continue;
 		}
 		/*if (scanner_msg.ranges[i] < range_min) {
@@ -126,20 +143,20 @@ void map_update(Vector3d &x) {
 	}
 }
 
-//Callback function for the Position topic (SIMULATION)
-void pose_callback(const gazebo_msgs::ModelStates &msg) {
-    int i;
-    for (i = 0; i < msg.name.size(); i++)
-        if (msg.name[i] == "mobile_base")
-            break;
-    ips_x = msg.pose[i].position.x;
-    ips_y = msg.pose[i].position.y;
-    ips_yaw = tf::getYaw(msg.pose[i].orientation);
-	if (first_pose) {
-		//map_msg.info.origin.orientation = msg.pose[i].orientation;
-		first_pose = false;
-	}
-}
+// //Callback function for the Position topic (SIMULATION)
+// void pose_callback(const gazebo_msgs::ModelStates &msg) {
+//     int i;
+//     for (i = 0; i < msg.name.size(); i++)
+//         if (msg.name[i] == "mobile_base")
+//             break;
+//     ips_x = msg.pose[i].position.x;
+//     ips_y = msg.pose[i].position.y;
+//     ips_yaw = tf::getYaw(msg.pose[i].orientation);
+// 	if (first_pose) {
+// 		//map_msg.info.origin.orientation = msg.pose[i].orientation;
+// 		first_pose = false;
+// 	}
+// }
 
 void scan_callback(const sensor_msgs::LaserScan &msg) {
 	//if (first_scan) {
@@ -147,26 +164,28 @@ void scan_callback(const sensor_msgs::LaserScan &msg) {
 		angle_max = msg.angle_max;
 		angle_increment = msg.angle_increment;
 		range_min = msg.range_min;
-		range_max = msg.range_max;      	
+		range_max = msg.range_max;  
+		// range_max = 3.0;    	
 		numRanges = (angle_max-angle_min)/angle_increment;
 		// numRanges = sizeof(msg.ranges)/4;
 		first_scan = false;
 		//ROS_INFO("angle_min = %f, angle_max = %f, range_min = %f, range_max = %f\n", angle_min, angle_max, range_min, range_max);
 	//} else {
 		scanner_msg.ranges = msg.ranges;
+		//ROS_INFO("Ranges: %f", scanner_msg.ranges[numRanges/2]);
 	//}
 }
 
-//Callback function for the Position topic (LIVE)
-/*
+Callback function for the Position topic (LIVE)
+
 void pose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
 {
 
-	ips_x X = msg.pose.pose.position.x; // Robot X psotition
-	ips_y Y = msg.pose.pose.position.y; // Robot Y psotition
+	ips_x  = msg.pose.pose.position.x; // Robot X psotition
+	ips_y  = msg.pose.pose.position.y; // Robot Y psotition
 	ips_yaw = tf::getYaw(msg.pose.pose.orientation); // Robot Yaw
-	ROS_DEBUG("pose_callback X: %f Y: %f Yaw: %f", X, Y, Yaw);
-}*/
+	//ROS_INFO("pose_callback X: %f Y: %f Yaw: %f", ips_x, ips_y, ips_yaw);
+}
 
 //Callback function for the map
 void map_callback(const nav_msgs::OccupancyGrid &msg) {
@@ -224,11 +243,12 @@ void bresenham(int x0, int y0, int x1, int y1, std::vector<int> &x, std::vector<
 
 int main(int argc, char **argv) {
     //Initialize the ROS framework
-    ros::init(argc, argv, "main_mapping");
+    ros::init(argc, argv, "main_control");
     ros::NodeHandle n;
 
     //Subscribe to the desired topics and assign callbacks
-    ros::Subscriber pose_sub = n.subscribe("/gazebo/model_states", 1, pose_callback);
+    // ros::Subscriber pose_sub = n.subscribe("/gazebo/model_states", 1, pose_callback);
+    ros::Subscriber pose_sub = n.subscribe("/indoor_pos", 10, pose_callback);
     ros::Subscriber map_sub = n.subscribe("/map", 1, map_callback);
 
 	// Subscribe to laser scans
@@ -237,11 +257,14 @@ int main(int argc, char **argv) {
     //Setup topics to Publish from this node
     ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
     pose_publisher = n.advertise<geometry_msgs::PoseStamped>("/pose", 1, true);
-    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
+    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10,true);
 	ros::Publisher map_publisher = n.advertise<nav_msgs::OccupancyGrid>("/map", 1, true);
 
-	tf::Transform transform;
-	tf::TransformBroadcaster br;
+	ros::Publisher path_pub = n.advertise<nav_msgs::Path>("/pathFlex",1,true);
+    geometry_msgs::PoseStamped path_pose;
+    std::vector<geometry_msgs::PoseStamped> path_vector;
+    nav_msgs::Path path_msg;
+    path_msg.header.frame_id = "map";;
 
     //Velocity control variable
     geometry_msgs::Twist vel;
@@ -260,22 +283,15 @@ int main(int argc, char **argv) {
 	map_msg.data.clear();
 	map_msg.header.frame_id = "map";
 	map_msg.info.map_load_time = ros::Time::now(); 
-	// map_msg.info.origin.position.x = X(0);
-	// map_msg.info.origin.position.y = X(1);
-	// map_msg.info.origin.position.z = X(2);
-	// ROS_INFO("Initial Positions x: %f, y: %f, z: %f", X(0), X(1), X(2));
-	geometry_msgs::Pose origin;
-	// map_msg.info.origin.position.x = -(M*RESOLUTION)/2.0;
-	// map_msg.info.origin.position.y = -(N*RESOLUTION)/2.0;
-	// map_msg.info.origin.position.z = 0;
 	map_msg.info.width = N;
 	map_msg.info.height = M;
 	map_msg.info.resolution = RESOLUTION;
 
+	geometry_msgs::Pose origin;
+
 	origin.position.x = -(M*RESOLUTION)/2.0;
 	origin.position.y = -(N*RESOLUTION)/2.0;
 	origin.position.z = 0; 
-
 	origin.orientation.x = 0;
 	origin.orientation.y = 0;
 	origin.orientation.z = 0;
@@ -289,6 +305,32 @@ int main(int argc, char **argv) {
         // vel.angular.z = 0.5; // set angular speed
         // velocity_publisher.publish(vel); // Publish the command velocity
         ros::spinOnce();   //Check for new messages
+
+        visualization_msgs::Marker line_strip; 
+        line_strip.header.frame_id = "map";
+        line_strip.header.stamp = ros::Time::now(); 
+        line_strip.ns = "points_and_lines";
+        line_strip.action = visualization_msgs::Marker::ADD;
+        line_strip.pose.orientation.w = 1.0; 
+
+        line_strip.id = 1;
+        
+        line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+  		
+	    // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
+	    line_strip.scale.x = 1;
+	    
+	     // Line strip is blue
+	    line_strip.color.b = 1.0;
+	    line_strip.color.a = 1.0;
+
+	    geometry_msgs::Point p;
+	    p.x = ips_x;
+	    p.y = ips_y;
+	    p.z = 0;
+	    line_strip.points.push_back(p);
+	    marker_pub.publish(line_strip);
+
 		X << ips_x, ips_y, ips_yaw; 
 
 		//ROS_INFO("angle_min = %f, angle_max = %f, range_min = %f, range_max = %f\n", angle_min, angle_max, range_min, range_max);
@@ -305,6 +347,12 @@ int main(int argc, char **argv) {
 	//	ROS_INFO("map_msg[%d]: %d\n" , M*N-1, map_msg.data[M*N-1]);  
 		map_msg.info.map_load_time = ros::Time::now(); 
 		map_publisher.publish(map_msg);	
+
+		path_pose.pose.position.x = ips_x;
+		path_pose.pose.position.y = ips_y;
+		path_vector.push_back(path_pose);
+		path_msg.poses = path_vector;
+		path_pub.publish(path_msg);
         loop_rate.sleep(); //Maintain the loop rate
     }
     return 0;
